0fc500226ad82464cb1b99e98271782347f010c2
Tajbir Hossain <mdtajbirhossain031@gmail.com>
2025-07-08 20:08:39 +0600

    Updated videos of hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,17 +14,17 @@ const videoUrl = [
   {
     // webm: "/videos/branding-portfolio.webm",
     // mp4: "/videos/branding-portfolio.mp4",
-    mp4: "/videos/design-and-branding.mp4"
+    mp4: "/videos/design-and-branding-2.mp4"
   },
   {
     // webm: "/videos/ads-portfolio.webm",
     // mp4: "/videos/ads-portfolio.mp4",
-    mp4: "/videos/marketing.mp4"
+    mp4: "/videos/marketing-2.mp4"
   },
   {
     // webm: "/videos/web-portfolio.webm",
     // mp4: "/videos/web-portfolio.mp4",
-    mp4: "/videos/web-development.mp4"
+    mp4: "/videos/web-development-4.mp4"
   },
 ]
 
