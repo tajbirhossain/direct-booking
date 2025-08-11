d06e65974781e95cff01e779fd48b5a3b12b6ef8
Tajbir Hossain <mdtajbirhossain031@gmail.com>
2025-08-11 23:54:53 +0600

    Final update done

---
+++ src/components/translation/GoogleTranslateLoader.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function GoogleTranslateLoader() {
+    useEffect(() => {
+        if (document.getElementById("google-translate-script")) return;
+
+        window.googleTranslateElementInit = () => {
+            new window.google.translate.TranslateElement(
+                { pageLanguage: "en" },
+                "google_translate_element"
+            );
+        };
+
+        const script = document.createElement("script");
+        script.id = "google-translate-script";
+        script.src =
+            "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        script.async = true;
+        document.body.appendChild(script);
+    }, []);
+
+    return <div id="google_translate_element" style={{ display: "none" }}></div>;
+}
